bc8a9cb98fb38ce7703001f313aea14c9cc98341
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-07-23 04:37:07 -0400

    style and create login form

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,12 +1,11 @@
 import Login from '@/components/Login';
-import Chat from '@/components/Chat';
+// import Chat from '@/components/Chat';
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
       <Login />
-      <Chat />
+      {/* <Chat /> */}
     </>
   );
 }
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,5 +1,26 @@
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
 const Login = () => {
-  return <p>Login Form</p>;
+  const login = () => {
+    console.log('login button pressed');
+  };
+
+  return (
+    <form action={login} className='grid h-full content-center gap-3 px-6'>
+      <Label htmlFor='password' className='text-lg'>
+        Password:
+      </Label>
+      <Input
+        type='password'
+        id='password'
+        name='password'
+        className='text-lg'
+      />
+      <Button className='text-lg'>Login</Button>
+    </form>
+  );
 };
 
 export default Login;
