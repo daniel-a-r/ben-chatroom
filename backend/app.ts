03503ab81d2c8b60a874d8bf904173d285f5172b
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-07-24 17:54:02 -0400

    create login endpoint

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,54 @@
-import Fastify from 'fastify';
+import Fastify, { FastifyRequest } from 'fastify';
+
+interface LoginBody {
+  password: string;
+}
 
 const fastify = Fastify({
-  logger: true,
+  logger: false,
 });
 
 fastify.get('/', async (_request, _reply) => {
+  console.log('server reached');
   return { hello: 'world' };
 });
+
+fastify.post(
+  '/login',
+  {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['password'],
+        properties: {
+          password: { type: 'string' },
+        },
+      },
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            message: { type: 'string' },
+            user: { type: 'string' },
+          },
+        },
+      },
+    },
+  },
+  (request: FastifyRequest<{ Body: LoginBody }>, reply) => {
+    console.log('login endpoint reached');
+    const { password } = request.body;
+    let user: string = '';
+    if (password === 'shannon') {
+      user = 'shannon';
+    } else if (password === 'kat') {
+      user = 'kat';
+    } else {
+      reply.code(400).send({ message: 'Invalid password' });
+    }
+    reply.send({ message: 'login successful', user });
+  },
+);
 
 try {
   await fastify.listen({ port: 3000 });
