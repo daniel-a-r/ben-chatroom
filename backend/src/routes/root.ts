2f5aeeb62f5c8078f99db686ba16729a41357f5b
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-07-24 21:30:36 -0400

    move route to routes folder

--- backend/src/routes/root.ts
+++ backend/src/routes/root.ts
@@ -1,9 +1,50 @@
-import { FastifyPluginAsync } from 'fastify';
+import { FastifyPluginAsync, FastifyReply, FastifyRequest } from 'fastify';
 
-const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
+interface LoginBody {
+  password: string;
+}
+
+const root: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
+  fastify.get('/', async function (_request, _reply) {
     return { root: true };
   });
+
+  fastify.post(
+    '/login',
+    {
+      schema: {
+        body: {
+          type: 'object',
+          required: ['password'],
+          properties: {
+            password: { type: 'string' },
+          },
+        },
+        response: {
+          200: {
+            type: 'object',
+            properties: {
+              message: { type: 'string' },
+              user: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+    (request: FastifyRequest<{ Body: LoginBody }>, reply: FastifyReply) => {
+      console.log('login endpoint reached');
+      const { password } = request.body;
+      let user: string = '';
+      if (password === 'shannon') {
+        user = 'shannon';
+      } else if (password === 'kat') {
+        user = 'kat';
+      } else {
+        return reply.code(400).send({ message: 'Invalid password' });
+      }
+      return reply.send({ message: 'login successful', user });
+    },
+  );
 };
 
 export default root;
