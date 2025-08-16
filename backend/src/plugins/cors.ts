1f55f1b5a181e6f6928b2c7b7b4e30886830d62b
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-08-16 01:23:57 -0400

    update cors

--- backend/src/plugins/cors.ts
+++ backend/src/plugins/cors.ts
@@ -3,7 +3,7 @@ import fastifyCors, { FastifyCorsOptions } from '@fastify/cors';
 
 export default fp<FastifyCorsOptions>((fastify) => {
   fastify.register(fastifyCors, {
-    origin: 'https://chatroom-3yb.pages.dev/',
+    origin: 'https://chatroom-3yb.pages.dev',
     methods: ['GET', 'POST'],
   });
 });
