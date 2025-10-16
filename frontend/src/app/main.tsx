d7ae40a5d0ea989cb9f91ab474cb71de6d5f3d09
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-10-16 01:20:38 -0400

    remove unused imports

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,12 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ThemeProvider } from '@/components/ThemeProvider';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
 import Provider from '@/app/Provider.tsx';
 import './index.css';
-
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
